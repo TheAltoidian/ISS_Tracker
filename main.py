dad34490d8adc428898eb92ad53305adccbeaba6
Cullen <cullenjcasey@gmail.com>
2024-02-24 19:48:45 -0800

    checks if the ISS is visible above

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 37.547150
+MY_LONG = -122.314400
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -10,6 +10,8 @@ data = response.json()
 
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
+
+
 
 #Your position is within +5 or -5 degrees of the ISS position.
 
@@ -26,10 +28,18 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+time_now = datetime.now().hour
+# print(f"iss_lat: {iss_latitude}\niss_lon: {iss_longitude}\ntime_now: {time_now}\nparameters : {parameters}\nsunrise: {sunrise}\nsunset: {sunset}")
 
-#If the ISS is close to my current position
-# and it is currently dark
+# Check if the ISS is overhead, with a 5 degree margin of error
+lat_distance = abs(iss_longitude - MY_LONG)
+lon_distance = abs(iss_latitude - MY_LAT)
+if lat_distance <= 5:
+    if lon_distance <= 5:
+        # Check if it's dark outside
+        if time_now >= sunset or time_now <= sunrise:
+            print("ISS is above!")
+
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
